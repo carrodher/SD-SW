63f41c788002bdc8f99996638971b34f710d46b9
carrodher <carrodher1179@gmail.com>
2016-02-28 19:48:05 +0100

    Modificaciones logs

--- practica3/2_log/cliente/ClienteLog.java
+++ practica3/2_log/cliente/ClienteLog.java
@@ -1,27 +1,35 @@
+/*
+ Cliente.
+ Obtiene la referencia remota asociada al servicio (a traves de rmiregistry). Invoca los métodos de forma normal, 
+ lo único diferente es que pueden generar la excepción RemoteException.
+*/
+
 import java.rmi.*;
 import java.rmi.server.*;
 
-
 class ClienteLog {
     static public void main (String args[]) {
-        if (args.length!=3) {
-            System.err.println("Uso: ClienteLog hostregistro numPuertoRegistro nombreCliente");
+        if (args.length != 3) {
+            System.err.println("Uso: ClienteLog hostregistro numPuertoRegistro mensajeLog");
             return;
         }
-
-       if (System.getSecurityManager() == null)
+        // Instancia gestor de seguridad
+        if (System.getSecurityManager() == null)
             System.setSecurityManager(new SecurityManager());
 
         try {
-
+            // Obtiene referencia remota del servicio de rmiregistry
             ServicioLog srv = (ServicioLog) Naming.lookup("//" + args[0] + ":" + args[1] + "/Log");
-    
+                                                            /*      |               |-> Número de puerto escucha rmiregistry
+                                                                    |-> Host en el que se ejecuta rmiregistry */   
             for (int i=0; i<10000; i++)
                 srv.log(args[2] + " " + i);
         }
+        // Excepción RMI
         catch (RemoteException e) {
             System.err.println("Error de comunicacion: " + e.toString());
         }
+        // Excepción cliente
         catch (Exception e) {
             System.err.println("Excepcion en ClienteLog:");
             e.printStackTrace();
--- practica3/2_log/servidor/ServicioLog.java
+++ practica3/2_log/servidor/ServicioLog.java
@@ -1,4 +1,10 @@
-
+/* 
+ (1)
+ Define una interfaz que deriva de la interfaz Remote y contiene los métodos requeridos
+ por el servicio, permitiendo usar la expcepción RemoteException que permite a RMI notificar
+ sobre errores relacionados con la comunicación.
+*/
+ 
 import java.rmi.*;
 
 interface ServicioLog extends Remote {
