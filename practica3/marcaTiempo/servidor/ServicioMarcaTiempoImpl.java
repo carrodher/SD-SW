e613eeccaa7fd6f7219b64c7f3ef67a0798efcab
carrodher <carrodher1179@gmail.com>
2016-02-28 19:47:34 +0100

    Servicio marca tiempo implementado

---
+++ practica3/marcaTiempo/servidor/ServicioMarcaTiempoImpl.java
@@ -0,0 +1,22 @@
+/*
+ (2)
+ Desarrollo la implementación de los servicios remotos.
+*/
+
+import java.io.*;
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.Date;
+
+class ServicioMarcaTiempoImpl extends UnicastRemoteObject implements ServicioMarcaTiempo {
+    ServicioMarcaTiempoImpl() throws RemoteException {
+    }
+
+    // Añadir synchronized para garantizar exclusión mútua entre los hilos al acceder al ḿétodo
+    public synchronized Date marcaTiempo() throws RemoteException {
+        // Obtenemos fecha y hora actual
+        Date date = new Date();
+
+        return date;
+    }
+}
