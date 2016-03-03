f7ed19c0ae317b33cc711cefc5d179b22a0d4eb0
carrodher <carrodher1179@gmail.com>
2016-03-03 18:33:03 +0100

    Apartado chat finalizado

--- practica4/3_chat/cliente/ClienteChat.java
+++ practica4/3_chat/cliente/ClienteChat.java
@@ -1,3 +1,9 @@
+/*
+ Cliente.
+ Obtiene la referencia remota asociada al servicio. Invoca los métodos de forma normal, 
+ lo único diferente es que pueden generar la excepción RemoteException.
+*/
+
 import java.util.*;
 import java.rmi.*;
 import java.rmi.server.*;
@@ -8,30 +14,38 @@ class ClienteChat {
             System.err.println("Uso: ClienteChat hostregistro numPuertoRegistro apodo");
             return;
         }
-
+        // Instancia gestor de seguridad
         if (System.getSecurityManager() == null){
             System.setSecurityManager(new SecurityManager());
         }
-
         try {
+        	// Obtiene referencia remota del servicio de rmiregistry
             ServicioChat srv = (ServicioChat) Naming.lookup("//" + args[0] + ":" + args[1] + "/Chat");
+                                                            /*      |               |-> Número de puerto escucha
+                                                                    |-> Host en el que se ejecuta el servicio*/
+            // Crea nuevo cliente y lo registra en la lista
             ClienteImpl c = new ClienteImpl();
-            srv.alta(c);
+            srv.alta(c);								// Llamada al método remoto 'alta' del servicio
+            
+            // Escribe y envía
             Scanner ent = new Scanner(System.in);
             String apodo = args[2];
             System.out.print(apodo + "> ");
             while (ent.hasNextLine()) {
-                srv.envio(c, apodo, ent.nextLine());
+                srv.envio(c, apodo, ent.nextLine());	// Llamada al método remoto 'envio' del servicio
                 System.out.print(apodo + "> ");
             }
-            srv.baja(c);
+            // Elimina de la lista al cliente
+            srv.baja(c);								// Llamada al método remoto 'baja' del servicio
             System.exit(0);
         }
+        // Excepción RMI
         catch (RemoteException e) {
-            System.err.println("Error de comunicacion: " + e.toString());
+            System.err.println("Error de comunicación: " + e.toString());
         }
+        // Excepción cliente
         catch (Exception e) {
-            System.err.println("Excepcion en ClienteChat:");
+            System.err.println("Excepción en ClienteChat:");
             e.printStackTrace();
         }
     }
--- practica4/3_chat/servidor/ServicioChatImpl.java
+++ practica4/3_chat/servidor/ServicioChatImpl.java
@@ -1,27 +1,33 @@
+/*
+ (2)
+ Desarrollo la implementación de los servicios remotos.
+*/
+
 import java.util.*;
 import java.rmi.*;
 import java.rmi.server.*;
 
 class ServicioChatImpl extends UnicastRemoteObject implements ServicioChat {
-    List<Cliente> l;
+    List<Cliente> listaClientes;                    // Lista de los clientes conectados
     ServicioChatImpl() throws RemoteException {
-        l = new LinkedList<Cliente>();
+        listaClientes = new LinkedList<Cliente>();
     }
 
+    // Añade un cliente a la lista
     public void alta(Cliente c) throws RemoteException {
-        l.add(c);
+        listaClientes.add(c);
     }
 
+    // Elimina un cliente de la lista
     public void baja(Cliente c) throws RemoteException {
-        l.remove(l.indexOf(c));
+        listaClientes.remove(listaClientes.indexOf(c));
     }
 
-    public void envio(Cliente esc, String apodo, String m){
-        throws RemoteException {
-            for (Cliente c: l) {
-                if (!c.equals(esc)){
-                    c.notificacion(apodo, m);
-                }
+    // Notifica a todos los clientes excepto escritor (esc)
+    public void envio(Cliente esc, String apodo, String m) throws RemoteException {
+        for (Cliente c: listaClientes) {
+            if (!c.equals(esc)){
+                c.notificacion(apodo, m);   // Llamada al método remoto 'notificacion'
             }
         }
     }
