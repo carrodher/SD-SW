43f5317065f16fc6d4bd2b08877982cc101c4cfc
carrodher <carrodher1179@gmail.com>
2016-03-14 18:11:11 +0100

    Banco finalizado

--- practica5/4_banco_simple/servidor/BancoImpl.java
+++ practica5/4_banco_simple/servidor/BancoImpl.java
@@ -1,9 +1,15 @@
+/*
+ (2)
+ Desarrollo la implementación de los servicios remotos.
+*/
+
 import java.rmi.*;
 import java.rmi.server.*;
 
 class BancoImpl extends UnicastRemoteObject implements Banco {
-    BancoImpl() throws RemoteException {
+	BancoImpl() throws RemoteException {
     }
+    // Crea un nuevo objeto que implementa la interfaz remota
     public Cuenta crearCuenta(String nombre) throws RemoteException {
         return new CuentaImpl(nombre);
     }
--- practica5/4_banco_simple/servidor/ServidorBanco.java
+++ practica5/4_banco_simple/servidor/ServidorBanco.java
@@ -1,3 +1,9 @@
+/*
+ (3)
+ Servidor. 
+ Inicia el servicio remoto y lo hace accesible de manera pública usando RMI.
+*/
+
 import java.rmi.*;
 import java.rmi.server.*;
 
@@ -7,17 +13,23 @@ class ServidorBanco  {
             System.err.println("Uso: ServidorBanco numPuertoRegistro");
             return;
         }
+        // Instancia gestor de seguridad
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new RMISecurityManager());
         }
         try {
+        	// Crea objeto de la clase que implementa el servicio remoto
             BancoImpl srv = new BancoImpl();
+            // Da de alta en rmi mediante rebind 
             Naming.rebind("rmi://localhost:" + args[0] + "/Banco", srv);
+            									// |-> Número de puerto escucha
         }
+        // Excepción RMI
         catch (RemoteException e) {
             System.err.println("Error de comunicacion: " + e.toString());
             System.exit(1);
         }
+        // Excepción servidor
         catch (Exception e) {
             System.err.println("Excepcion en ServidorBanco:");
             e.printStackTrace();
