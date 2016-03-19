f3bc6e22d49e99cf6cd3a9e2be5f97551a1895ca
carrodher <carrodher1179@gmail.com>
2016-03-19 19:11:21 +0100

    Comentarios y estilo Logs

--- practica6/fabricalogcompleta/servidor/Log.java
+++ practica6/fabricalogcompleta/servidor/Log.java
@@ -1,19 +1,30 @@
 import java.io.*;
 
+/* Clase Log. Alamcena la información (nombre del fichero e ID de la aplicación)
+de los objetos del Servicio de Log */
 class Log implements Serializable {
-     private String nombreFichero;
-     private String idApp;
-     Log(String nomFich, String id) {
-         nombreFichero = nomFich;
-         idApp = id;
-     }
-     public String obtenerNombreFichero() {
-         return nombreFichero;
-     }
-     public String obtenerIdApp() {
-         return idApp;
-     }
-     public String toString() {
-         return nombreFichero + " | " + idApp;
-     }
+    private String nombreFichero;
+    private String idApp;
+
+    // Constructor de la clase log. Recibe el nombre del fichero y un ID de app
+    Log(String nomFich, String id) {
+        nombreFichero = nomFich;
+        idApp = id;
+    }
+
+    /* Métodos */
+    // Devuelve el nombre del fichero
+    public String obtenerNombreFichero() {
+        return nombreFichero;
+    }
+
+    // Devuelve el ID de la aplicación
+    public String obtenerIdApp() {
+        return idApp;
+    }
+
+    // Devuelve el nombre y el ID como una única cadena
+    public String toString() {
+        return nombreFichero + " | " + idApp;
+    }
 }
--- practica6/fabricalogcompleta/servidor/ServicioLog.java
+++ practica6/fabricalogcompleta/servidor/ServicioLog.java
@@ -1,10 +1,12 @@
-
 import java.rmi.*;
 import java.util.Date;
 
+/* (3)
+Define una interfaz que deriva de la interfaz Remote y contiene los métodos requeridos
+por el servicio, permitiendo usar la expcepción RemoteException que permite a RMI notificar
+sobre errores relacionados con la comunicación. */
 interface ServicioLog extends Remote {
 	void log (String m) throws RemoteException;
 	Log obtenerLog() throws RemoteException;
 	Date obtenerFecha() throws RemoteException;
 }
-
