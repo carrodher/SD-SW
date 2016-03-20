a8883a222e1a177b7d653fe5d26a4ec17d511beb
carrodher <carrodher1179@gmail.com>
2016-03-20 21:05:22 +0100

    Finalizado cliente

--- practica6/fabricalogcompleta/cliente/ClienteLog.java
+++ practica6/fabricalogcompleta/cliente/ClienteLog.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.rmi.*;
 import java.rmi.server.*;
 
@@ -6,8 +7,11 @@ Invoca los métodos de forma normal, lo único diferente es que pueden generar
 la excepción RemoteException. */
 class ClienteLog {
     static public void main (String args[]) {
+        int tamLista = 0;
+
         if (args.length != 4) {
-            System.err.println("Uso: ClienteLog hostregistro numPuertoRegistro nombreLog mensajeLog");
+            //                                     args[0]         args[1]      args[2]  args[3]
+            System.err.println("Uso: ClienteLog hostregistro numPuertoRegistro nombreLog IDapp");
             return;
         }
         // Instancia gestor de seguridad
@@ -16,12 +20,41 @@ class ClienteLog {
         }
         try {
             // Obtiene referencia remota del servicio de rmiregistry
-            FabricaLog srv = (FabricaLog) Naming.lookup("//" + args[0] + ":" + args[1] + "/FabricaLog");
-            /*      |               |-> Número de puerto escucha rmiregistry
-            |-> Host en el que se ejecuta rmiregistry */
-            ServicioLog servLog = srv.crearLog(args[2]);
-            for (int i=0; i<1000; i++)
-            servLog.log(args[3] + " " + i);
+            FabricaLog fabrica = (FabricaLog) Naming.lookup("//" + args[0] + ":" + args[1] + "/FabricaLog");
+            //                                                   |               |-> Número de puerto escucha rmiregistry
+            //                                                   |-> Host en el que se ejecuta rmiregistry
+
+            // Crea nuevo objeto log. Párametros: nombreLog e IDapp
+            Log logCliente = new Log(args[2], args[3]);
+            // Crea el servicio de Log asociado al Log creado anteriormente
+            ServicioLog servLog = fabrica.crearLog(logCliente);
+            System.out.println("Fichero | IDapp          Fecha Creación \n");
+            while (true) {
+                // Cada 3" registra el log
+                try {
+                    Thread.sleep(3000);
+                }
+                catch(InterruptedException e) {
+                    System.err.println(e);
+                    System.exit(1);
+                }
+                // Realiza el registro periódico
+                servLog.log(logCliente.toString());
+
+                // Obtiene la nueva lista de todos los Servicios de Log que hay en el momento
+                List <ServicioLog> lista = fabrica.obtenerLogs();
+                // Si ha habido cambios en la lista...
+                if (tamLista != lista.size()){
+                    // Recorre toda la lista de logs
+                    for (ServicioLog i: lista){
+                        //logCliente = i.obtenerLog();
+                        System.out.println(i.obtenerLog() + " => " + i.obtenerFecha());
+                    }
+                    System.out.println("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+                }
+                // Tamaño de la lista antes de actualziar
+                tamLista = lista.size();
+            }
         }
         // Excepción RMI
         catch (RemoteException e) {
