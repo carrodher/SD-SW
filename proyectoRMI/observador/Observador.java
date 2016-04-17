4a6da5f2867c3dbc114330db7c81051f858abf37
Carlos RH <carrodher1179@gmail.com>
2016-04-17 22:58:56 +0200

    :lipstick: Añadir comentario

--- proyectoRMI/observador/Observador.java
+++ proyectoRMI/observador/Observador.java
@@ -1,8 +1,12 @@
 import java.rmi.*;
 
-// Interfaz observador
+/* (1)
+Define una interfaz que derive de la interfaz Remote y contiene los métodos requeridos
+por el servicio, permitiendo usar la expcepción RemoteException que permite a RMI notificar
+sobre errores relacionados con la comunicación.
+
+Métodos relativos a las funcionalidades de los observadores */
 interface Observador extends Remote {
 	void servicioSolicitado(String nombre, String tlfn, String address, String mensaje) throws RemoteException;
-	void setNombre(String n) throws RemoteException;
 	String getNombre() throws RemoteException;
 }
