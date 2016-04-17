3ea5afbc337b3bea9fda55bcfa6202a8933a889e
Carlos RH <carrodher1179@gmail.com>
2016-04-17 22:55:51 +0200

    :lipstick: Añadir comentario

--- proyectoRMI/centralita/ServicioCentralita.java
+++ proyectoRMI/centralita/ServicioCentralita.java
@@ -3,7 +3,9 @@ import java.rmi.*;
 /* (1)
 Define una interfaz que derive de la interfaz Remote y contiene los métodos requeridos
 por el servicio, permitiendo usar la expcepción RemoteException que permite a RMI notificar
-sobre errores relacionados con la comunicación. */
+sobre errores relacionados con la comunicación.
+
+Métodos relativos a las funciones que realiza la centralita. */
 interface ServicioCentralita extends Remote {
     void addObservador(Observador o, String nombre) throws RemoteException;
     void delObservador(Observador o, String nombre) throws RemoteException;
