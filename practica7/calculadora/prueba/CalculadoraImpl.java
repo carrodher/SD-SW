53b3ccf448f7daca4b664f68dcd011dc4c43774a
Carlos RH <carrodher1179@gmail.com>
2016-05-07 12:40:32 +0200

    :lipstick: Cambiar indentación y añadir comentarios

--- practica7/calculadora/prueba/CalculadoraImpl.java
+++ practica7/calculadora/prueba/CalculadoraImpl.java
@@ -2,48 +2,53 @@ package prueba;
 
 import prueba.*;
 
-class CalculadoraImpl extends CalculadoraPOA
-{
-  private double memoria_;
-  public CalculadoraImpl()
-  {
-    memoria_ = 0;
-  }
-  public double add(double x, double y)
-  {
-    return x + y;
-  }
+/* El sirviente es un objeto Java que implementa la funcionalidad de los métodos del objeto
+CORBA. Este sirviente es llamado por el soporte del servidor cuando un cliente llama a un
+método del objeto CORBA implementado por ese sirviente. El código del sirviente implementa
+las funciones de la calculadora*/
+class CalculadoraImpl extends CalculadoraPOA {
+    private double memoria_;
 
-  public double substract(double x, double y)
-  {
-    return x - y;
-  }
-  public double multiply(double x, double y)
-  {
-    return x * y;
-  }
-
-  public double divide(double x, double y)
-  {
-    double result = 0;
-
-    try
-    {
-      result = x / y;
-    } catch(Exception e)
-    {
+    // Constructor
+    public CalculadoraImpl() {
+        memoria_ = 0;
     }
 
-    return result;
-  }
-  public void storeMemory(double x)
-  {
-    memoria_ = x;
-  }
+    // Suma
+    public double add(double x, double y) {
+        return x+y;
+    }
 
-  public double readMemory()
-  {
-    return memoria_;
-  }
+    // Resta
+    public double substract(double x, double y) {
+        return x-y;
+    }
+
+    // Multiplica
+    public double multiply(double x, double y) {
+        return x*y;
+    }
+
+    // Divide
+    public double divide(double x, double y) {
+        double result = 0;
+
+        try {
+            result = x/y;
+        }
+        catch(Exception e) {
+        }
+
+        return result;
+    }
+
+    // Escribe en la memoria
+    public void storeMemory(double x) {
+        memoria_ = x;
+    }
+
+    // Lee de la memoria
+    public double readMemory() {
+        return memoria_;
+    }
 }
-
--- practica7/calculadora/prueba/Cliente.java
+++ practica7/calculadora/prueba/Cliente.java
@@ -4,31 +4,31 @@ import prueba.*;
 import org.omg.CORBA.*;
 import java.io.*;
 
-public class Cliente
-{
-  public static void main(String args[])
-  {
-    try
-    {
-      // Iniciar el ORB
-      org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
+/* Por último queda implementar el cliente que use la referencia del objeto remoto para pedir sus
+servicios. En este caso, el cliente obtiene la referencia del objeto remoto usando el fichero que ha
+escrito el servidor con su referencia */
+public class Cliente {
+    public static void main(String args[]) {
+        try {
+            // Iniciar el ORB (igual que el servidor)
+            org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
 
-      // Leer el IOR del fichero
-      File IORFile = new File("IOR");
-      FileReader reader = new FileReader(IORFile);
-      BufferedReader buf = new BufferedReader(reader);
-      String IOR = buf.readLine();
+            // Leer el IOR del fichero
+            File IORFile = new File("IOR");
+            FileReader reader = new FileReader(IORFile);
+            BufferedReader buf = new BufferedReader(reader);
+            String IOR = buf.readLine();
 
-      // Convertir el IOR en un objeto
-      org.omg.CORBA.Object o = orb.string_to_object(IOR);
-      Calculadora calc = CalculadoraHelper.narrow(o);
+            /* Convertir el IOR en un objeto: A partir del string que se ha leído del fichero, se obtiene la
+            referencia al objeto usando el método string_to_object del orb */
+            org.omg.CORBA.Object o = orb.string_to_object(IOR);
+            Calculadora calc = CalculadoraHelper.narrow(o);
 
-      // Usar la calculadora
-      System.out.println(calc.add(2.0, 3.0));
-    } catch(Exception e)
-    {
-        e.printStackTrace();
+            // Usar la calculadora
+            System.out.println(calc.add(2.0, 3.0));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
-
