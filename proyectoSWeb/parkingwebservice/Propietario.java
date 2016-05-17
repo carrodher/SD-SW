4ba2d669ca65a5f8b1707accf740a905504fa5fa
Carlos RH <carrodher1179@gmail.com>
2016-05-17 21:29:20 +0200

    :penguin: nuevos toString en propietario

--- proyectoSWeb/parkingwebservice/Propietario.java
+++ proyectoSWeb/parkingwebservice/Propietario.java
@@ -1,66 +1,71 @@
 package parkingwebservice;
 
 public class Propietario implements java.io.Serializable {
-    private String nombre;
-    private String apellidos;
-    private String dni;
-    private String telefono;
-    private boolean abonado = false;		// Abonado => No tarifica
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private String telefono;
+	private boolean abonado = false;		// Abonado => No tarifica
 
 	/* Métodos set */
-    // Establece el dni
-    public void setDni(String d) {
-        this.dni = d;
-    }
+	// Establece el dni
+	public void setDni(String d) {
+		this.dni = d;
+	}
 
-    // Establece el nombre
-    public void setNombre(String n) {
-        this.nombre = n;
-    }
+	// Establece el nombre
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
 
-    // Establece los apellidos
-    public void setApellidos(String a) {
-        this.apellidos = a;
-    }
+	// Establece los apellidos
+	public void setApellidos(String a) {
+		this.apellidos = a;
+	}
 
-    // Establece el telefono
-    public void setTelefono(String t) {
-        this.telefono = t;
-    }
+	// Establece el telefono
+	public void setTelefono(String t) {
+		this.telefono = t;
+	}
 
-    // Establece si tiene plaza
-    public void setAbono(boolean b) {
-        this.abonado = b;
-    }
+	// Establece si tiene plaza
+	public void setAbono(boolean b) {
+		this.abonado = b;
+	}
 
 	/* Métodos get */
-    // Obtiene el dni
-    public String getDni() {
-        return this.dni;
-    }
+	// Obtiene el dni
+	public String getDni() {
+		return this.dni;
+	}
 
-    // Obtiene el nombre
-    public String getNombre() {
-        return this.nombre;
-    }
+	// Obtiene el nombre
+	public String getNombre() {
+		return this.nombre;
+	}
 
-    // Obtiene los apellidos
-    public String getApellidos() {
-        return this.apellidos;
-    }
+	// Obtiene los apellidos
+	public String getApellidos() {
+		return this.apellidos;
+	}
 
-    // Obtiene el telefono
-    public String getTelefono() {
-        return this.telefono;
-    }
+	// Obtiene el telefono
+	public String getTelefono() {
+		return this.telefono;
+	}
 
-    // Obtiene si tiene plaza
-    public boolean getAbono() {
-        return this.abonado;
-    }
+	// Obtiene si tiene plaza
+	public boolean getAbono() {
+		return this.abonado;
+	}
 
-    // Devuelve el nombre y el DNI como una única cadena
-    public String toString() {
-        return this.nombre + " | " + this.dni;
-    }
+	// Devuelve el nombre y los apellidos como una única cadena
+	public String nombreComToString() {
+		return this.nombre +  this.apellidos + " | " + this.dni;
+	}
+
+	// Devuelve el nombre y el DNI como una única cadena
+	public String nombreDniToString() {
+		return this.nombre + " | " + this.dni;
+	}
 }
